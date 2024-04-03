3be7a4046edf4319195d702f26250ee852951fec
Zachary Kulis <zkulis@kultek.com>
2024-04-02 23:14:02 -0400

    Ensure unique folder for downloaded/patched chromedriver to prevent conflicts.

--- undetected_chromedriver/patcher.py
+++ undetected_chromedriver/patcher.py
@@ -10,6 +10,7 @@ import pathlib
 import platform
 import random
 import re
+import secrets
 import shutil
 import string
 import sys
@@ -59,7 +60,7 @@ class Patcher(object):
         """
         self.force = force
         self._custom_exe_path = False
-        prefix = "undetected"
+        prefix = secrets.token_hex(8)
         self.user_multi_procs = user_multi_procs
 
         self.is_old_chromedriver = version_main and version_main <= 114
